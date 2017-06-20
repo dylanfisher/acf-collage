46e41988c09767c2aace89b16dd5d866c7a9a33c
dylanfisher <hi@dylanfisher.com>
2017-06-20 00:08:44 -0400

    Undo

--- fields/acf-collage-v5.php
+++ fields/acf-collage-v5.php
@@ -191,9 +191,6 @@ class acf_field_collage extends acf_field {
             $ratio = 16 / 9;
 
             $row_layout = isset( $collage_item['acf_fc_layout'] ) ? $collage_item['acf_fc_layout'] : 'custom_row';
-            if ( empty( $row_layout ) ) {
-              $row_layout = 'custom_row';
-            }
 
             if ( $row_layout == 'image' ) {
               $image = $collage_item['image'];
